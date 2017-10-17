ca1d5713b5ed188be3f8b58b162ef969312ddab4
seanqureshi <sean.m.qureshi@gmail.com>
2017-10-17 14:31:07 -0600

    filled out

--- server/index.js
+++ server/index.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `${__dirname}/controllers/messages_controller` );
+const session = require('express-session');
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../public/build` ) );
+app.use( session({
+    secret: 'dfsdfhsjkfhsajkfhjd_thing',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 10000000
+    }
+}))
+app.use( createInitialSession ); 
+app.use( (req, res, next) => {
+    if (req.method === 'POST' || req.method === 'PUT'){
+        filter(req, res, next);
+    }
+    next();
+})
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
-app.get( messagesBaseUrl, mc.read );
+app.get( "/api/messages/history", mc.history );
+app.get( messagesBaseUrl, mc.read )
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
 
